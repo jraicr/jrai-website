bea902d027ae2520012797fa4ce0c69d13113ced
J. Rai <jraicr@gmail.com>
2023-10-23 18:26:59 +0100

    Add astrojs/partytown dependency

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from 'astro/config';
-
 import vue from "@astrojs/vue";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue()]
+  integrations: [vue(), partytown({
+    config: {
+      forward: ["dataLayer.push"],
+    },
+  })]
 });
