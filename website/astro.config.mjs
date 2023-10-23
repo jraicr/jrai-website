70060f326462d8400581f1d3815e06a612389714
J. Rai <jraicr@gmail.com>
2023-10-23 19:05:48 +0100

    Add astrojs/sitemap dependency

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,11 +1,20 @@
 import { defineConfig } from 'astro/config';
 import vue from "@astrojs/vue";
-
+import sitemap from "@astrojs/sitemap";
 import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue(), partytown({
+  site: 'https://www.jrai.dev',
+  integrations: [vue(), sitemap({
+    i18n: {
+      defaultLocale: 'es', // All urls that don't contain `en` or `fr` after `https://jrai.dev/` will be treated as default locale,
+      locales: {
+        es: 'es-ES', // The `defaultLocale` value must present in `locales` keys
+        en: 'en-US', 
+      },
+    },
+  }), partytown({
     config: {
       forward: ["dataLayer.push"],
     },
